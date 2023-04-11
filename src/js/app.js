f4338fd471ea542dd37a8227f9457ead541b8c81
Maciej_Zieminski <ziemac1@gmail.com>
2023-04-11 13:51:04 +0200

    Finish module 9

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function () {
@@ -79,11 +80,18 @@ const app = {
     });
   },
 
+  initBooking: function() {
+    const thisApp = this;
+    const bookingWrapper = document.querySelector(select.containerOf.booking);
+    new Booking(select.containerOf.booking);
+  },
+
   init: function(){
     const thisApp = this;
     thisApp.initPages();  
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,34 @@
+import { templates, select } from "../settings";
+import AmountWidget from "./AmountWidget";
+
+class Booking {
+    constructor(element) {
+        const thisApp = this;
+        thisApp.render(element);
+        thisApp.initWidgets();
+    }
+
+    render (element) {
+        const thisApp = this;
+        const generatedHTML = templates.bookingWidget;
+        thisBooking.dom = {};
+        thisBooking.dom.wrapper = element;
+        thisBooking.dom.wrapper.innerHTML = generatedHTML;
+        thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+        thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    }
+
+    initWidgets () {
+        const thisApp = this;
+        thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.dom.peopleAmount.addEventListener('updated', function(){
+        });
+    
+        thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+        });
+    }
+
+}
+
+export default Booking;
